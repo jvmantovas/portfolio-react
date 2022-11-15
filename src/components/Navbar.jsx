f3362709ad8f679a12bc0139c4e0d5d0f6bb0634
jvmantovas <vmsjoao@gmail.com>
2022-11-15 19:31:34 -0300

    Navbar sizing adjusts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,9 +10,9 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-8 bg-[#0a192f] text-gray-300">
       <div>
-        <img src={logo} alt="Logo" style={{ width: "50px" }} />
+        <img src={logo} alt="Logo" style={{ width: "100px" }} />
       </div>
       {/* menu */}
       <ul className="hidden md:flex">
@@ -43,7 +43,7 @@ const Navbar = () => {
         </li>
       </ul>
       {/*Hamburguer*/}
-      <div onClick={handleClick} className="md:hidden z-10">
+      <div onClick={handleClick} className="md:hidden z-10 scale-150">
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
       {/*mobile menu*/}
