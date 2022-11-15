f71a0edea4f379383b747908c16dd5608e23f56f
jvmantovas <vmsjoao@gmail.com>
2022-11-15 15:18:58 -0300

    Navbar component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div>
-      <h2>Alou</h2>
       <Navbar />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import logo from "../assets/logo.png";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
         <img src={logo} alt="Logo" style={{ width: "50px" }} />
       </div>
-      <div>{/* menu */}</div>
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>Sobre</li>
-          <li>Habilidades</li>
-          <li>Trabalho</li>
-          <li>Contato</li>
-        </ul>
-      </div>
-
+      {/* menu */}
+      <ul className="hidden md:flex">
+        <li>Home</li>
+        <li>Sobre</li>
+        <li>Habilidades</li>
+        <li>Trabalho</li>
+        <li>Contato</li>
+      </ul>
       {/*Hamburguer*/}
-      <div>
-        <FaBars />
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
       </div>
       {/*mobile menu*/}
-      <div></div>
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">Sobre</li>
+        <li className="py-6 text-4xl">Habilidades</li>
+        <li className="py-6 text-4xl">Trabalho</li>
+        <li className="py-6 text-4xl">Contato</li>
+      </ul>
       {/*social icons*/}
-      <div></div>
+      <div className="hidden"></div>
     </div>
   );
 };
