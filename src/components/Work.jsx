2cdb12ee696e3bfc72c496334484e28d0776c783
jvmantovas <vmsjoao@gmail.com>
2022-11-15 19:55:16 -0300

    style and typo fix

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,15 +4,18 @@ import realEstate from "../assets/projects/realestate.jpg";
 
 const Work = () => {
   return (
-    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+    <div
+      name="work"
+      className="w-full md:h-full pt-20 text-gray-300 bg-[#0a192f]"
+    >
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600 ">
             Trabalhos
           </p>
           {/*eslint-disable-next-line react/jsx-no-comment-textnodes*/}
           <p className="py-6">
-            // Configura alguns dos projetos recentes em que trabalhei
+            // Confira alguns projetos recentes em que trabalhei
           </p>
         </div>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
