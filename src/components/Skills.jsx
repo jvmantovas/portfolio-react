1da47b46e8851a6856e6a4f56f6bc42688c26779
jvmantovas <vmsjoao@gmail.com>
2022-11-15 19:55:33 -0300

    style fix

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,7 +9,10 @@ import Tailwind from "../assets/tailwind.png";
 
 const Skills = () => {
   return (
-    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
+    <div
+      name="skills"
+      className="w-full h-screen bg-[#0a192f] text-gray-300 px-8 pt-20"
+    >
       {/*Container*/}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
