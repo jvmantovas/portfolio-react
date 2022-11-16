84ea2dbad9319a39fbe6ab1de5044c1ee72e7cdd
jvmantovas <vmsjoao@gmail.com>
2022-11-15 22:37:22 -0300

    Navbar logo fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo.gif";
 import { Link } from "react-scroll";
 
 const Navbar = () => {
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-8 bg-[#0a192f] text-gray-300">
       <div>
-        <img src={logo} alt="Logo" style={{ width: "100px" }} />
+        <img src={logo} alt="Logo" style={{ width: "200px" }} />
       </div>
       {/* menu */}
       <ul className="hidden md:flex">
