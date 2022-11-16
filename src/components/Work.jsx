89ab514726d9a8ef6045f2718a59329ad3a7f625
jvmantovas <vmsjoao@gmail.com>
2022-11-16 04:28:23 -0300

    Quickpay project added

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Pokedex from "../assets/projects/pokedex.png";
+import Quickpay from "../assets/projects/quickpay.png";
 
 const Work = () => {
   return (
@@ -18,15 +19,16 @@ const Work = () => {
           </p>
         </div>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* project card */}
           <div
             style={{ backgroundImage: `url(${Pokedex})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div relative hover:scale-105 duration-300"
           >
-            <div className="absolute w-full h-full bg-fixed rounded-md opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out bg-[#102747d3] border-2 border-[#F4B325] text-center">
+            <div className="absolute w-full h-full bg-fixed rounded-md opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out bg-[#102747d3] border-2 border-[#F4B325] text-center pt-2">
               <span className="text-2xl font-bold text-white tracking-wider">
                 PokéDex Web App
               </span>
-              <div className="max-h-[170px] font-semibold">
+              <div className="font-semibold text-sm py-4 px-2">
                 <p>
                   A PokéDex é um aplicativo web que consome a{" "}
                   <a
@@ -45,7 +47,7 @@ const Work = () => {
                   </p>
                 </p>
               </div>
-              <div className="text-center max-h-10 flex space-x-4 justify-center pt-2">
+              <div className="absolute bottom-3 left-16 text-center max-h-10 flex space-x-4 pt-2">
                 <a
                   href="https://pokedex-js-jvmantovas.vercel.app/"
                   rel="noreferrer"
@@ -67,36 +69,29 @@ const Work = () => {
               </div>
             </div>
           </div>
+          {/* project card */}
           <div
-            style={{ backgroundImage: `url(${Pokedex})` }}
+            style={{ backgroundImage: `url(${Quickpay})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div relative hover:scale-105 duration-300"
           >
-            <div className="absolute w-full h-full bg-fixed rounded-md opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out bg-[#102747d3] border-2 border-[#F4B325] text-center">
+            <div className="absolute w-full h-full bg-fixed rounded-md opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out bg-[#102747d3] border-2 border-[#F4B325] text-center pt-2">
               <span className="text-2xl font-bold text-white tracking-wider">
-                PokéDex Web App
+                Quickpay CC Validator
               </span>
-              <div className="max-h-[170px] font-semibold">
+              <div className=" font-semibold text-sm py-4 px-2">
                 <p>
-                  A PokéDex é um aplicativo web que consome a{" "}
-                  <a
-                    href="https://pokeapi.co/"
-                    rel="noreferrer"
-                    target="_blank"
-                    className="text-blue-500"
-                  >
-                    pokeAPI
-                  </a>{" "}
-                  para gerar os cards com os pokemons. Neste projeto utilizei o
-                  método fetch API do JavaScript para buscar os dados e gerar os
-                  cards HTML dinamicamente.
+                  O Quickpay é um componente que simula o formulário de
+                  preenchimento de cartão de crédito, onde é possível adicionar
+                  máscara (Imask regex) aos inputs e atualizar elementos HTML
+                  via DOM.
                   <p className="font-bold text-blue-400">
-                    #html #css #bootstrap #javascript
+                    #html #css #javascript #node #vite #imask
                   </p>
                 </p>
               </div>
-              <div className="text-center max-h-10 flex space-x-4 justify-center pt-2">
+              <div className="absolute bottom-3 left-16 text-center max-h-10 flex space-x-4 pt-2">
                 <a
-                  href="https://pokedex-js-jvmantovas.vercel.app/"
+                  href="http://quickpay-cc-checker-js.vercel.app/"
                   rel="noreferrer"
                   target="_blank"
                 >
@@ -105,56 +100,7 @@ const Work = () => {
                   </button>
                 </a>
                 <a
-                  href="https://github.com/jvmantovas/pokedex-js"
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  <button className="text-center h-full px-4 rounded-lg bg-[#F4B325] text-gray-700 font-bold text-lg hover:scale-110 duration-300">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${Pokedex})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div relative hover:scale-105 duration-300"
-          >
-            <div className="absolute w-full h-full bg-fixed rounded-md opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out bg-[#102747d3] border-2 border-[#F4B325] text-center">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                PokéDex Web App
-              </span>
-              <div className="max-h-[170px] font-semibold">
-                <p>
-                  A PokéDex é um aplicativo web que consome a{" "}
-                  <a
-                    href="https://pokeapi.co/"
-                    rel="noreferrer"
-                    target="_blank"
-                    className="text-blue-500"
-                  >
-                    pokeAPI
-                  </a>{" "}
-                  para gerar os cards com os pokemons. Neste projeto utilizei o
-                  método fetch API do JavaScript para buscar os dados e gerar os
-                  cards HTML dinamicamente.
-                  <p className="font-bold text-blue-400">
-                    #html #css #bootstrap #javascript
-                  </p>
-                </p>
-              </div>
-              <div className="text-center max-h-10 flex space-x-4 justify-center pt-2">
-                <a
-                  href="https://pokedex-js-jvmantovas.vercel.app/"
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  <button className="text-center h-full px-4 rounded-lg bg-[#F4B325] text-gray-700 font-bold text-lg hover:scale-110 duration-300">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/jvmantovas/pokedex-js"
+                  href="https://github.com/jvmantovas/quickpay-cc-checker-js"
                   rel="noreferrer"
                   target="_blank"
                 >
