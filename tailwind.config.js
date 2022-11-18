42fcb49f2818accf8fdc84c8cd3e1038ffd8633f
jvmantovas <vmsjoao@gmail.com>
2022-11-18 00:29:51 -0300

    tailwind fontsize config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontSize: {
+      sm: "0.85rem",
+      base: "1rem",
+    },
   },
   plugins: [],
 };
