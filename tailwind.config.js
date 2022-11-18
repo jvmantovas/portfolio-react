18220868551bb7bb8164dbbf21372804031c4b5a
jvmantovas <vmsjoao@gmail.com>
2022-11-18 00:43:13 -0300

    tailwind config fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-    fontSize: {
-      sm: "0.85rem",
-      base: "1rem",
-    },
-  },
+  theme: {},
   plugins: [],
 };
